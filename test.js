e4b97ea3ace288b3a501b30714add567ea6a891e
Moti Zilberman <motiz88@gmail.com>
2015-11-25 17:58:20 +0200

    Refactor and add more meaningful tests

--- test.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert');
-var fs = require('fs');
-var praat = require('./');
-
-assert(fs.existsSync(praat), 'Praat not found at ' + praat);
-console.log('Praat found at ' + praat);
